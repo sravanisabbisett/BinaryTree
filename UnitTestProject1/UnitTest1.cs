61e6bf65b1648c164aa92776fd259e8083fe279b
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-08 15:52:04 +0530

    ADDED:Testcase to create the binary tree with 13 elements

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -25,5 +25,25 @@ namespace UnitTestProject1
             int size = tree.Size();
             Assert.AreEqual(3, size);
         }
+
+        [TestMethod]
+        public void Adding13ElementsInBinaryTreeShouldReturnSize13()
+        {
+            tree.Add(56);
+            tree.Add(30);
+            tree.Add(70);
+            tree.Add(22);
+            tree.Add(40);
+            tree.Add(60);
+            tree.Add(95);
+            tree.Add(11);
+            tree.Add(3);
+            tree.Add(16);
+            tree.Add(65);
+            tree.Add(63);
+            tree.Add(67);
+            int size = tree.Size();
+            Assert.AreEqual(13, size);
+        }
     }
 }
